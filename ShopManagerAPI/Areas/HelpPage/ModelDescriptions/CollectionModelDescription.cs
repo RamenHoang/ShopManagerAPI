f10df53b555416ef6fa4aa8496ec78252122cd99
RamenHoang <anhnguyenhoang321@gmail.com>
2020-07-21 13:39:04 +0700

    Tinh chinh project, them gitignore

--- ShopManagerAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ShopManagerAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
