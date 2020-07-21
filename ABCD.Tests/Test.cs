39623abcceab9ae9d7f31eb5c20eb976c25bddd7
Nguyễn Anh <ramen@Nguyens-MacBook-Pro.local>
2020-07-22 01:04:53 +0700

    FIST

---
+++ ABCD.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ABCD.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
